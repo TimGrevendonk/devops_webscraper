e4609f4daa802af4ccefa9dea45634949aef39ae
Tim Grevendonk <71647518+TimGrevendonk@users.noreply.github.com>
2021-12-02 14:37:49 +0100

    devops basic programme

---
+++ webscraper_devops/webscraper/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("webscraper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("webscraper")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ca7a99c-5e7a-4fd6-94ca-17a331b0cae4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ webscraper_devops/webscraper/webscraper.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace webscraper
+{
+    public class Webscraper
+    {
+        public static void Main(string[] args)
+        {
+                // Use the chromedriver.exe in the Drivers folder.
+            ChromeDriver driver = new ChromeDriver(@"../../" + "/Drivers/");
+            driver.Manage().Window.Maximize();
+                // set the timeout value to 10 seconds (will wait until items are loaded for a max of 10 seconds).
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                // Go to the website (YouTube in this case)
+            driver.Navigate().GoToUrl("https://www.youtube.com/");
+                // Click "accept Terms" button.
+            driver.FindElement(By.XPath("//*[@id='content']/div[2]/div[5]/div[2]/ytd-button-renderer[2]")).Click();
+
+            Console.WriteLine("helloz?");
+                // sleep/wait for 1 second to let the items/page load, or else the input field is not fillable.
+            System.Threading.Thread.Sleep(1000);
+                // Find the Youtube input field.
+            IWebElement searchBarInput = driver.FindElement(By.Name("search_query"));
+                // Enter value in the field and submit it.
+            searchBarInput.SendKeys("rick roll");
+            searchBarInput.Submit();   
+        }
+    }
+}
